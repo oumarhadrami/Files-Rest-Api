1623dab3d358f76a24011038580d07303ddc4c70
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2021-03-05 18:11:22 +0000

    Remove request mapping standard path to api

--- src/main/java/com/mohamed/hadramy/springbootapp/controller/FilesController.java
+++ src/main/java/com/mohamed/hadramy/springbootapp/controller/FilesController.java
@@ -14,17 +14,15 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
-@RestController
-@RequestMapping("/files_api/v1/")
+@Controller
 public class FilesController {
 
   @Autowired
